ea64ddebceb271cf1f38cd266c670a2cb6659eb0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-08-20 22:01:37 +0200

    Create composables needed to comunicate with Firebase

---
+++ src/composables/useLogout.js
@@ -0,0 +1,22 @@
+import { ref } from 'vue';
+import { projectAuth } from '../firebase/config';
+
+// refs
+const error = ref(null);
+
+// logout function
+const logout = async () => {
+  error.value = null;
+
+  try {
+    await projectAuth.signOut();
+  }
+  catch (err) {
+    console.log(err.message);
+    error.value = err.message;
+  }
+}
+
+const useLogout = () => ({ error, logout });
+
+export default useLogout;

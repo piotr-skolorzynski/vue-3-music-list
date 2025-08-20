53131d1b53835597a55a999e794e4e031f039a32
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-08-20 21:51:40 +0200

    Create connection to Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgLovb7mJuMdTlpPej-SNq5YnToBRGkLw",
+    authDomain: "vue-3-music-list.firebaseapp.com",
+    projectId: "vue-3-music-list",
+    storageBucket: "vue-3-music-list.firebasestorage.app",
+    messagingSenderId: "869260250095",
+    appId: "1:869260250095:web:b06caaf8cce00694abc41a"
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };

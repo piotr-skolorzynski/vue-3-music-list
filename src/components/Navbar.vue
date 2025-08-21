1a31481bf0f2ed04714fe70ff4b051f2b8e35f1a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-08-21 08:54:57 +0200

    Conditionally show links in Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,13 +4,35 @@
       <img src="@/assets/ninja.png" alt="logo" />
       <h1><router-link :to="{ name: 'home' }">Muso Ninjas</router-link></h1>
       <div class="links">
-        <button>Logout</button>
-        <router-link class="btn" :to="{ name: 'Signup' }">Sign up</router-link>
-        <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        <div v-if="user">
+          <button @click="handleLogout">Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">
+            Sign up
+          </router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        </div>
       </div>
     </nav>
   </div>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import useLogout from '@/composables/useLogout';
+import getUser from '@/composables/getUser';
+
+const router = useRouter();
+const { logout } = useLogout();
+const { user } = getUser();
+
+const handleLogout = async () => {
+  await logout();
+  console.log('user logged out');
+  router.push({ name: 'Login' });
+};
+</script>
 
 <style scoped>
 .navbar {
@@ -30,15 +52,15 @@ nav h1 {
   margin-left: 20px;
 }
 
-nav .links {
-  margin-left: auto;
-}
-
 nav .links,
 a,
 button {
   margin-left: 16px;
   font-size: 14px;
+}
+
+nav .links {
+  margin-left: auto;
 }
 
 nav img {
